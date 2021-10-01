0f248accd97588354b27287604d41859f25362d8
benpaquier <hello@benpaquier.com>
2021-10-01 19:18:54 +0200

    Fix

--- src/App.js
+++ src/App.js
@@ -1,135 +1,141 @@
-import { useEffect, useState } from "react"
-import moment from "moment"
-import uniq from "lodash/uniq"
-import first from "lodash/first"
+// import { useEffect, useState } from "react"
+// import moment from "moment"
+// import uniq from "lodash/uniq"
+// import first from "lodash/first"
 
-import { Container, ListGroup, Spinner } from "react-bootstrap"
-import { getCourses, getStudents } from "./api"
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import { Container, ListGroup, Spinner } from "react-bootstrap"
+// import { getCourses, getStudents } from "./api"
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
 
-const App = () => {
-  console.log(first)
+// const App = () => {
+//   console.log(first)
 
-  const [loading, setLoading] = useState(true)
-  const [ranking, setRanking] = useState([])
-  const [courses, setCourses] = useState([])
-  const [students, setStudents] = useState([])
+//   const [loading, setLoading] = useState(true)
+//   const [ranking, setRanking] = useState([])
+//   const [courses, setCourses] = useState([])
+//   const [students, setStudents] = useState([])
 
-  useEffect(() => {
-    fetchCourses()
-  }, [])
+//   useEffect(() => {
+//     fetchCourses()
+//   }, [])
 
-  useEffect(() => {
-    // getStudentsNames
-    if (courses.length > 0) {
-      fetchStudentNames()
-    }
-  // eslint-disable-next-line
-  }, [courses])
+//   useEffect(() => {
+//     // getStudentsNames
+//     if (courses.length > 0) {
+//       fetchStudentNames()
+//     }
+//   // eslint-disable-next-line
+//   }, [courses])
 
-  useEffect(() => {
-    if (students.length > 0) {
-      doRanking()
-    }
-  // eslint-disable-next-line
-  }, [students])
+//   useEffect(() => {
+//     if (students.length > 0) {
+//       doRanking()
+//     }
+//   // eslint-disable-next-line
+//   }, [students])
   
-  const fetchCourses = async () => {
-    const response = await getCourses()
-    setCourses(response)
-  }
+//   const fetchCourses = async () => {
+//     const response = await getCourses()
+//     setCourses(response)
+//   }
 
-  const fetchStudentNames = async () => {
-    let uniqueIds = courses.map(course => (
-      course.students.map(s => s.studentId)
-    )) || []
+//   const fetchStudentNames = async () => {
+//     let uniqueIds = courses.map(course => (
+//       course.students.map(s => s.studentId)
+//     )) || []
 
-    uniqueIds = uniq(uniqueIds.flat())
+//     uniqueIds = uniq(uniqueIds.flat())
 
-    const response = await getStudents(uniqueIds)
-    setStudents(response)
-    setLoading(false)
-  }
+//     const response = await getStudents(uniqueIds)
+//     setStudents(response)
+//     setLoading(false)
+//   }
 
-  const doRanking = () => {
-    console.log(courses)
-    const ranks = []
+//   const doRanking = () => {
+//     console.log(courses)
+//     const ranks = []
  
-    courses.forEach(course => {
-      course.students.forEach(s => {
-        const existingRank = ranks.find(rank => rank.id === s.studentId)
+//     courses.forEach(course => {
+//       course.students.forEach(s => {
+//         const existingRank = ranks.find(rank => rank.id === s.studentId)
 
-        if (existingRank) {
-          const newRank = {
-            ...existingRank,
-            delay: existingRank.delay + s.delay
-          }
+//         if (existingRank) {
+//           const newRank = {
+//             ...existingRank,
+//             delay: existingRank.delay + s.delay
+//           }
 
-          const rankIndex = ranks.findIndex(r => r.id === existingRank.id)
-          ranks[rankIndex] = newRank
-        } else {
-          ranks.push({
-            id: s.studentId,
-            delay: s.delay
-          })
-        }
-      })
-    })
+//           const rankIndex = ranks.findIndex(r => r.id === existingRank.id)
+//           ranks[rankIndex] = newRank
+//         } else {
+//           ranks.push({
+//             id: s.studentId,
+//             delay: s.delay
+//           })
+//         }
+//       })
+//     })
 
-    setRanking(ranks)
-  }
+//     setRanking(ranks)
+//   }
 
-  if (loading) {
-    return (
-      <Container className="d-flex justify-content-center pt-5">
-        <Spinner animation="border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
-      </Container>
-    )
-  }
+//   if (loading) {
+//     return (
+//       <Container className="d-flex justify-content-center pt-5">
+//         <Spinner animation="border" role="status">
+//           <span className="visually-hidden">Loading...</span>
+//         </Spinner>
+//       </Container>
+//     )
+//   }
 
-  if (courses.length > 0 && students.length > 0 && ranking.length > 0) {
-    return (
-      <Container className="mt-5 mb-5">
-        <h2>🏆 Classement retards 🏆</h2>
-        <ListGroup className="mb-5">
-          {ranking.sort((a, b) => b.delay - a.delay).map((rank, i) => (
-            <ListGroup.Item>
-              <p>
-                <b>
-                  {i === 0 && `🥇`}
-                  {i === 1 && `🥈`}
-                  {i === 2 && `🥉`}
-                  {i + 1}.
-                </b>
-                {` `}
-                {students.find(s => s.ID === rank.id).FIRSTNAME} {students.find(s => s.ID === rank.id).LASTNAME}: {rank.delay}mn
-              </p>
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
-        <h2>Détails</h2>
-        <ListGroup>
-          {courses.map(course => (
-            <ListGroup.Item>
-              <p><b>{moment(course.start).format("D-MM-YY hh:mm")} à {moment(course.end).format("hh:mm")}</b></p>
-              <ul>
-                {course.students.map(student => {
-                  return (
-                    <li>{students.find(s => s.ID === student.studentId).FIRSTNAME} {students.find(s => s.ID === student.studentId).LASTNAME}: {student.delay}mn</li>
-                  )
-                })}
-              </ul>
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
-      </Container>
-    )
-  } else {
-    return null
-  }
+//   if (courses.length > 0 && students.length > 0 && ranking.length > 0) {
+//     return (
+//       <Container className="mt-5 mb-5">
+//         <h2>🏆 Classement retards 🏆</h2>
+//         <ListGroup className="mb-5">
+//           {ranking.sort((a, b) => b.delay - a.delay).map((rank, i) => (
+//             <ListGroup.Item>
+//               <p>
+//                 <b>
+//                   {i === 0 && `🥇`}
+//                   {i === 1 && `🥈`}
+//                   {i === 2 && `🥉`}
+//                   {i + 1}.
+//                 </b>
+//                 {` `}
+//                 {students.find(s => s.ID === rank.id).FIRSTNAME} {students.find(s => s.ID === rank.id).LASTNAME}: {rank.delay}mn
+//               </p>
+//             </ListGroup.Item>
+//           ))}
+//         </ListGroup>
+//         <h2>Détails</h2>
+//         <ListGroup>
+//           {courses.map(course => (
+//             <ListGroup.Item>
+//               <p><b>{moment(course.start).format("D-MM-YY hh:mm")} à {moment(course.end).format("hh:mm")}</b></p>
+//               <ul>
+//                 {course.students.map(student => {
+//                   return (
+//                     <li>{students.find(s => s.ID === student.studentId).FIRSTNAME} {students.find(s => s.ID === student.studentId).LASTNAME}: {student.delay}mn</li>
+//                   )
+//                 })}
+//               </ul>
+//             </ListGroup.Item>
+//           ))}
+//         </ListGroup>
+//       </Container>
+//     )
+//   } else {
+//     return null
+//   }
+// }
+
+// export default App
+
+export default () => {
+  return (
+    <h1>Hello there!</h1>
+  )
 }
-
-export default App
